dc7c70bf99ebc64bff17f9ba24f2112bc3d996de
WolgaDeutsche <myxa.zhuk@gmail.com>
2023-12-27 20:01:53 +0300

    0.1

---
+++ script.js
@@ -0,0 +1,89 @@
+  /* Открыть */
+  function openNav() {
+    document.getElementById("ratenav").style.height = "100%";
+  }
+
+  /* Закрыть */
+  function closeNav() {
+    document.getElementById("ratenav").style.height = "0%";
+  }
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function org() {
+    document.getElementById("org").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  function reg() {
+    document.getElementById("reg").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  function year() {
+    document.getElementById("year").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+    function openNav1() {
+      document.getElementById("filternav").style.height = "100%";
+    }
+  
+    function closeNav1() {
+      document.getElementById("filternav").style.height = "0%";
+    }
+  
+    // Accordion open/close
+    var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
